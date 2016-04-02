5a7edad53c2ba9280847ebf0df844792b6b4789c
Wassim Benadel <contact@wassimbenadel.com>
2016-04-02 22:56:06 +0200

    Added via upload

---
+++ contact.php
@@ -0,0 +1,16 @@
+ <?php 
+    $firstname=$_POST['firstname'];
+    $lastname=$_POST['lastname']; 
+    $email=$_POST['email']; 
+    $message=$_POST['message']; 
+    if (($firstname=="")||($lastname=="")||($email=="")||($message=="")) 
+    { 
+    echo "All fields are required, please fill <a href=\"\">the form</a> again."; 
+    } 
+    else {         
+    $from="From: $firstname $lastname <$email>\r\nReturn-path: $email"; 
+    $subject="Message sent using 1988 contact form"; 
+    mail("contact@wassimbenadel.com", $subject, $message, $from); 
+    echo "<h4 class='text-center'>Thank you for contacting us, we will respond withing 24 hours!</h4>"; 
+    }
+?>
